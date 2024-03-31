54f7ee0765ac76274364f7c7037e363344c3343d
Mingkun <164994306+Ming-kun@users.noreply.github.com>
2024-03-31 10:22:08 -0500

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -11,14 +11,14 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
-  STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
+  STARTER_HERO_TITLE_1: '创新意味着孤身走暗巷，没有同伴的支持，没有市场的反馈。所有的确定感，都来自你心中的火苗', // 英雄区文字
+  STARTER_HERO_TITLE_2: 'Don’t be the best, Be the only.', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+  STARTER_HERO_BUTTON_1_TEXT: '盗火', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'https://space.bilibili.com/8819422/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '明坤', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://www.linkedin.com/in/mingkunzheng/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_ICON: '/images/starter/linkedin.svg', // 英雄区按钮2的图标，不需要则留空
 
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
