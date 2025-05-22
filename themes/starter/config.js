b5dda10774effd2b72461d93f21194993261e8a7
Mingkun <164994306+Ming-kun@users.noreply.github.com>
2025-05-22 16:24:52 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -14,8 +14,7 @@ const CONFIG = {
   STARTER_HERO_TITLE_2: 'Skin in the Game', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: 'Linkedin', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL:
-    'www.linkedin.com/in/mingkunzheng', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'www.linkedin.com/in/mingkunzheng', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: 'Subscribe on YouTube', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://www.youtube.com/@AAA_Lab', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/youtube.svg', // 英雄区按钮2的图标，不需要则留空
@@ -33,31 +32,30 @@ const CONFIG = {
 
   // 特性区块
   STARTER_FEATURE_ENABLE: true, // 特性区块开关
-  STARTER_FEATURE_TITLE: '特性', // 特性
-  STARTER_FEATURE_TEXT_1: 'NotionNext的主要特性', // 特性
+  STARTER_FEATURE_TITLE: 'How to Get Started?​​', // 特性
+  STARTER_FEATURE_TEXT_1: '4 Paths to AGI', // 特性
   STARTER_FEATURE_TEXT_2:
-    'NotionNext的愿景是帮助您简单、无感知地稳定地搭建自己的网站，放大品牌的价值。 ', // 特性
+    'AI works for you—you’re in the driver’s seat. You may steer a great car, but your grit outpaces the machine.', // 特性
 
-  STARTER_FEATURE_1_TITLE_1: '免费且开源', // 特性1
+  STARTER_FEATURE_1_TITLE_1: 'AGI', // 特性1
   STARTER_FEATURE_1_TEXT_1: '项目源码在Github上完全开放共享，遵循MIT协议', // 特性1
   STARTER_FEATURE_1_BUTTON_TEXT: '了解更多', // 特性1
-  STARTER_FEATURE_1_BUTTON_URL: 'https://github.com/tangly1024/NotionNext', // 特性1
+  STARTER_FEATURE_1_BUTTON_URL: '/tag/AGI', // 特性1
 
-  STARTER_FEATURE_2_TITLE_1: '多种主题定制', // 特性2
+  STARTER_FEATURE_2_TITLE_1: 'AI Agent', // 特性2
   STARTER_FEATURE_2_TEXT_1: '数十种主题,适用于不同场景，总有一款适合你', // 特性2
   STARTER_FEATURE_2_BUTTON_TEXT: '了解更多', // 特性2
-  STARTER_FEATURE_2_BUTTON_URL:
-    'https://docs.tangly1024.com/article/notion-next-themes', // 特性2
+  STARTER_FEATURE_2_BUTTON_URL: '/tag/AI%20Agent', // 特性2
 
-  STARTER_FEATURE_3_TITLE_1: '优秀的性能', // 特性3
+  STARTER_FEATURE_3_TITLE_1: 'AI Art', // 特性3
   STARTER_FEATURE_3_TEXT_1: '基于NextJS开发，更快的响应速度，更好的SEO', // 特性3
   STARTER_FEATURE_3_BUTTON_TEXT: '了解更多', // 特性3
-  STARTER_FEATURE_3_BUTTON_URL: 'https://docs.tangly1024.com/article/next-js', // 特性3
+  STARTER_FEATURE_3_BUTTON_URL: '/tag/AI%20Art', // 特性3
 
-  STARTER_FEATURE_4_TITLE_1: '便捷的写作体验', // 特性4
+  STARTER_FEATURE_4_TITLE_1: 'Smart Home', // 特性4
   STARTER_FEATURE_4_TEXT_1: '只需在Notion笔记中编修，自动同步到网站', // 特性4
   STARTER_FEATURE_4_BUTTON_TEXT: '了解更多', // 特性4
-  STARTER_FEATURE_4_BUTTON_URL: 'https://docs.tangly1024.com/about', // 特性4
+  STARTER_FEATURE_4_BUTTON_URL: '/tag/Smart%20Home', // 特性4
 
   // 首页ABOUT区块
   STARTER_ABOUT_ENABLE: true, // ABOUT区块开关
