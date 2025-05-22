e0c5ac22833d2877d3a251baeb68f7cec5ad12c2
Mingkun <164994306+Ming-kun@users.noreply.github.com>
2025-05-22 16:37:20 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -38,23 +38,23 @@ const CONFIG = {
     'AI works for you—you’re in the driver’s seat. You may steer a great car, but your grit outpaces the machine.', // 特性
 
   STARTER_FEATURE_1_TITLE_1: 'AGI', // 特性1
-  STARTER_FEATURE_1_TEXT_1: '项目源码在Github上完全开放共享，遵循MIT协议', // 特性1
-  STARTER_FEATURE_1_BUTTON_TEXT: '了解更多', // 特性1
+  STARTER_FEATURE_1_TEXT_1: 'Humanity’s dream roommate—masters every task, never steals your coffee', // 特性1
+  STARTER_FEATURE_1_BUTTON_TEXT: 'Learn more', // 特性1
   STARTER_FEATURE_1_BUTTON_URL: '/tag/AGI', // 特性1
 
   STARTER_FEATURE_2_TITLE_1: 'AI Agent', // 特性2
-  STARTER_FEATURE_2_TEXT_1: '数十种主题,适用于不同场景，总有一款适合你', // 特性2
-  STARTER_FEATURE_2_BUTTON_TEXT: '了解更多', // 特性2
+  STARTER_FEATURE_2_TEXT_1: 'An invisible intern that automates chores and never asks for pay', // 特性2
+  STARTER_FEATURE_2_BUTTON_TEXT: 'Learn more', // 特性2
   STARTER_FEATURE_2_BUTTON_URL: '/tag/AI%20Agent', // 特性2
 
   STARTER_FEATURE_3_TITLE_1: 'AI Art', // 特性3
-  STARTER_FEATURE_3_TEXT_1: '基于NextJS开发，更快的响应速度，更好的SEO', // 特性3
-  STARTER_FEATURE_3_BUTTON_TEXT: '了解更多', // 特性3
+  STARTER_FEATURE_3_TEXT_1: 'Code-wielding Picasso: zero paint, unlimited pixels, instant gallery', // 特性3
+  STARTER_FEATURE_3_BUTTON_TEXT: 'Learn more', // 特性3
   STARTER_FEATURE_3_BUTTON_URL: '/tag/AI%20Art', // 特性3
 
   STARTER_FEATURE_4_TITLE_1: 'Smart Home', // 特性4
-  STARTER_FEATURE_4_TEXT_1: '只需在Notion笔记中编修，自动同步到网站', // 特性4
-  STARTER_FEATURE_4_BUTTON_TEXT: '了解更多', // 特性4
+  STARTER_FEATURE_4_TEXT_1: 'A wise-cracking Wi-Fi butler that nags, then kills the lights before you can say “Ohm', // 特性4
+  STARTER_FEATURE_4_BUTTON_TEXT: 'Learn more', // 特性4
   STARTER_FEATURE_4_BUTTON_URL: '/tag/Smart%20Home', // 特性4
 
   // 首页ABOUT区块
