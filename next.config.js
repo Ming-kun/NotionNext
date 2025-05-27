de88eee876d4f1ba99eee9253a8e0f943299c841
Mingkun <164994306+Ming-kun@users.noreply.github.com>
2025-05-27 11:00:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -226,6 +226,15 @@ const nextConfig = {
     THEMES: themes
   }
 }
+nextConfig.rewrites = async () => {
+  return [
+    // 1) 中文站所有路径 → 中文项目域名
+    {
+      source: '/cn/:path*',
+      destination: 'https://notion-next-cn.vercel.app/:path*'
+    }
+  ];
+};
 
 module.exports = process.env.ANALYZE
   ? withBundleAnalyzer(nextConfig)
