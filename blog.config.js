e34f40aa1fb708ae69acc85b9d714ddd0f9aa617
Mingkun <164994306+Ming-kun@users.noreply.github.com>
2025-05-28 14:44:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,11 +16,11 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Mingkun', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'The Path to AGI', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://stealingfire.cc', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://stealingfire.cc/cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog，Stealingfire', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
-  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://stealingfire.cc/', // 备案查询链接，如果用了萌备等备案请在这里填写
+  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || '', // 备案查询链接，如果用了萌备等备案请在这里填写
 
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
